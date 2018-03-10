abbd45bc31faac8bc0c30afbb8d84697d389c6f0
Łukasz Antończyk <antonczyklukasz@gmail.com>
2018-03-10 18:11:26 +0100

    ClientServer performs request from session

--- apps/fury/lib/fury/client_server.ex
+++ apps/fury/lib/fury/client_server.ex
@@ -31,6 +31,7 @@ defmodule Fury.ClientServer do
 
     case Client.connect(transport_mod, url) do
       {:ok, transport} ->
+        make_request()
         {:noreply, %{state | transport: transport}}
 
       {:error, _} ->
@@ -47,8 +48,39 @@ defmodule Fury.ClientServer do
   def handle_info(:transport_disconnected, state) do
     {:noreply, %{state | transport: :not_connected}}
   end
+  def handle_info({:make_request, id}, %{transport: :not_connected} = state) do
+    make_request_after(id, :timer.seconds(5))
+
+    {:noreply, state}
+  end
+  def handle_info({:make_request, request_id}, state) do
+    %{transport_mod: transport_mod, transport: transport,
+      protocol_mod: protocol_mod, session_id: session_id} = state
+
+    case Session.get_request(session_id, request_id) do
+      {:ok, {:think, time}} ->
+        make_request_after(request_id + 1, :timer.seconds(time))
+        {:noreply, state}
+
+      {:ok, request} ->
+        Client.make_request(transport_mod, transport, protocol_mod, request)
+        make_request(request_id + 1)
+        {:noreply, state}
+
+      {:error, _} ->
+        {:noreply, state}
+    end
+  end
+
 
   defp connect do
     send(self(), :connect)
+  end
+
+  defp make_request(id \\ 0) do
+    send(self(), {:make_request, id})
+  end
+  defp make_request_after(id , time) do
+    Process.send_after(self(), {:make_request, id}, time)
   end
 end
--- apps/fury/test/fury/client_server_test.exs
+++ apps/fury/test/fury/client_server_test.exs
@@ -5,7 +5,7 @@ defmodule Fury.ClientServerTest do
 
   alias Fury.ClientServer
   alias Fury.ClientServer.State
-  alias Fury.Mock.{Protocol, Transport}
+  alias Fury.Mock.{Protocol, Storm, Transport}
 
   setup :set_mox_global
   setup do
@@ -60,6 +60,16 @@ defmodule Fury.ClientServerTest do
         {:noreply, %{state | transport: pid}}
     end
 
+    test "starts making requests when transport connects", %{state: state} do
+      stub Transport, :connect, fn _, _->
+        {:ok, self()}
+      end
+
+      ClientServer.handle_info(:connect, state)
+
+      assert_receive {:make_request, 0}
+    end
+
     test "on error sets transport to :not_connected", %{state: state} do
       pid = self()
       expect Transport, :connect, fn "localhost", [client: ^pid] ->
@@ -68,6 +78,16 @@ defmodule Fury.ClientServerTest do
 
       assert ClientServer.handle_info(:connect, state) ==
         {:noreply, %{state | transport: :not_connected}}
+    end
+
+    test "does not make requests on transport error", %{state: state} do
+      stub Transport, :connect, fn _, _->
+        {:error, :timeout}
+      end
+
+      ClientServer.handle_info(:connect, state)
+
+      refute_receive {:make_request, 0}
     end
   end
 
@@ -86,6 +106,92 @@ defmodule Fury.ClientServerTest do
     test "sets transport to :not_connected", %{state: state} do
       assert ClientServer.handle_info(:transport_disconnected, state) ==
         {:noreply, %{state | transport: :not_connected}}
+    end
+  end
+
+  describe "handle_info({:make_request, id} state)" do
+    setup %{state: state} do
+      # stub Protocol, :format, fn _, _ -> %{} end
+      # stub Transport, :push, fn _, _ -> :ok end
+      # stub Storm, :get_request, fn _, _ -> {:error, :not_found} end
+
+      {:ok, state: %{state | transport: self()}}
+    end
+
+    test "returns unmodified state", %{state: state} do
+      stub Storm, :get_request, fn _, _ -> {:error, :not_found} end
+
+      assert ClientServer.handle_info({:make_request, 0}, state) ==
+        {:noreply, state}
+    end
+
+    test "invokes StormBridge to get request", %{state: state} do
+      expect Storm, :get_request, fn _, _ -> {:error, :not_found} end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      verify!()
+    end
+
+
+    test "performs received request", %{state: state} do
+      stub Storm, :get_request, fn _, _ -> {:ok, {:push, "data"}} end
+      expect Protocol, :format, fn {:push, "data"}, _ -> {:ok, "data"} end
+      expect Transport, :push, fn _, "data" -> :ok end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      verify!()
+    end
+
+    test "makes next request", %{state: state} do
+      stub Storm, :get_request, fn _, _ -> {:ok, {:push, "data"}} end
+      stub Protocol, :format, fn _, _ -> {:ok, "data"} end
+      stub Transport, :push, fn _, _ -> :ok end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      assert_receive {:make_request, 1}
+    end
+
+    test "does not make request on {:think, time}", %{state: state} do
+      pid = self()
+      stub Storm, :get_request, fn _, _ -> {:ok, {:think, 1}} end
+      stub Protocol, :format, fn _, _ -> send(pid, :protocol_called) end
+      stub Transport, :push, fn _, _ -> send(pid, :tranport_called) end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      refute_receive _
+    end
+
+    test "waits before next request on {:think, time}", %{state: state} do
+      stub Storm, :get_request, fn _, _ -> {:ok, {:think, 0}} end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      refute_received {:make_request, 1}
+      assert_receive {:make_request, 1}
+    end
+
+    test "does not make request when there aren't any", %{state: state} do
+      pid = self()
+      stub Storm, :get_request, fn _, _ -> {:error, :not_found} end
+      stub Protocol, :format, fn _, _ -> send(pid, :protocol_called) end
+      stub Transport, :push, fn _, _ -> send(pid, :tranport_called) end
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      refute_receive _
+    end
+
+    test "does not make request when not connected", %{state: state} do
+      stub Storm, :get_request, fn _, _ -> send(self(), :bridge_called) end
+      state = %{state | transport: :not_connected}
+
+      ClientServer.handle_info({:make_request, 0}, state)
+
+      refute_receive _
     end
   end
 end
