ff847c681c6d1428f671ce26c427eea9f5d8b68f
Łukasz Antończyk <antonczyklukasz@gmail.com>
2018-03-11 17:55:57 +0100

    Fury: make client and session supervisors module based

--- apps/fury/lib/fury/client_supervisor.ex
+++ apps/fury/lib/fury/client_supervisor.ex
@@ -1,13 +1,19 @@
 defmodule Fury.ClientSupervisor do
+  use DynamicSupervisor
+
   alias Fury.ClientServer
 
   def start_link(_) do
-    DynamicSupervisor.start_link(strategy: :one_for_one, name: __MODULE__)
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def start_child(transport_mod, protocol_mod, session_id) do
     child_spec = {ClientServer, [transport_mod, protocol_mod, session_id]}
 
     DynamicSupervisor.start_child(__MODULE__, child_spec)
+  end
+
+  def init(_) do
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
--- apps/fury/lib/fury/session_supervisor.ex
+++ apps/fury/lib/fury/session_supervisor.ex
@@ -1,13 +1,19 @@
 defmodule Fury.SessionSupervisor do
+  use DynamicSupervisor
+
   alias Fury.SessionServer
 
   def start_link(_) do
-    DynamicSupervisor.start_link(strategy: :one_for_one, name: __MODULE__)
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def start_child(id, url, transport_mod, protocol_mod) do
     child_spec = {SessionServer, [id, url, transport_mod, protocol_mod]}
 
     DynamicSupervisor.start_child(__MODULE__, child_spec)
+  end
+
+  def init(_) do
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
