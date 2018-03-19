7606cad815d1cd39ade469b79f11539299a43af9
Łukasz Antończyk <antonczyklukasz@gmail.com>
2018-03-19 19:23:01 +0100

    Db: remove duplicated getting record fields

--- apps/db/lib/db/table.ex
+++ apps/db/lib/db/table.ex
@@ -2,19 +2,8 @@ defmodule Db.Table do
   alias Db.Record
 
   def create_from_struct(%mod{} = struct, opts \\ []) do
-    opts = [{:attributes, table_attributes(struct)} | opts]
+    opts = [{:attributes, Record.record_keys(struct)} | opts]
 
     :mnesia.create_table(mod, opts)
-  end
-
-  defp table_attributes(struct) do
-    fields =
-      struct
-      |> Map.from_struct()
-      |> Map.delete(:id)
-      |> Map.keys()
-      |> Enum.sort
-
-    [:id | fields]
   end
 end
