d85fface32944182944215c743b9c4558d1ba222
Łukasz Antończyk <antonczyklukasz@gmail.com>
2018-03-21 22:13:17 +0100

    Storm: helper for starting slave nodes

--- apps/storm/config/config.exs
+++ apps/storm/config/config.exs
@@ -1,3 +1,6 @@
 use Mix.Config
 
+config :storm, :host, System.get_env("STORMFURY_HOST") ||
+  raise "STORMFURY_HOST environment variable must be set!"
+
 import_config "#{Mix.env}.exs"
---
+++ apps/storm/lib/storm/nodes.ex
@@ -0,0 +1,52 @@
+defmodule Storm.Nodes do
+  @storm_host Application.fetch_env!(:storm, :host)
+
+  def start_node(host) do
+    host
+    |> to_charlist()
+    |> allow_inet_boot()
+    |> start_slave()
+    |> case do
+         {:ok, node} -> start_fury(node)
+         error -> error
+       end
+  end
+
+  def stop_node(node) do
+    :slave.stop(node)
+  end
+
+  defp allow_inet_boot(host) do
+    {:ok, ipv4} = :inet.parse_ipv4_address(host)
+    :erl_boot_server.add_slave(ipv4)
+
+    host
+  end
+
+  defp start_slave(host) do
+    :slave.start(host, :fury, beam_args())
+  end
+
+  defp beam_args do
+    cookie = :erlang.get_cookie()
+
+    to_charlist("-loader inet -hosts #{@storm_host} -setcookie #{cookie}")
+  end
+
+  defp start_fury(node) do
+    with :ok <- load_paths(node),
+         {:ok, _} <- start_application(node) do
+      {:ok, node}
+    else
+      error -> error
+    end
+  end
+
+  defp load_paths(node) do
+    :rpc.block_call(node, :code, :add_paths, [:code.get_path()])
+  end
+
+  defp start_application(node) do
+    :rpc.block_call(node, Application, :ensure_all_started, [:fury])
+  end
+end
