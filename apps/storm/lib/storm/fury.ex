09b15d8723b48fd15e884b65bd7849ab8ed9990b
Łukasz Antończyk <antonczyklukasz@gmail.com>
2018-04-30 09:27:59 +0200

    Storm: don't start remote simulation on current node in prod

--- apps/storm/lib/storm/fury.ex
+++ apps/storm/lib/storm/fury.ex
@@ -7,11 +7,18 @@ defmodule Storm.Fury do
   def start_simulation(id, sessions) do
     request = {:start_simulation, id, sessions}
 
-    GenServer.multi_call(Node.list(:known), Fury.Server, request, @timeout)
+    GenServer.multi_call(nodes(Mix.env()), Fury.Server, request, @timeout)
   end
 
   @impl true
   def start_clients(pid, session_id, ids) do
     GenServer.cast(pid, {:start_clients, session_id, ids})
+  end
+
+  defp nodes(:prod) do
+    Node.list()
+  end
+  defp nodes(_) do
+    Node.list(:known)
   end
 end
